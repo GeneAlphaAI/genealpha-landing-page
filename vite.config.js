d6a473c0b14461b7ed1cedf318cbe7008e09b9fc
Luigi <faizan@genealpha.ai>
2025-07-11 12:45:02 +0500

    Updates the port number

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5179, // change here
+    allowedHosts: ["genealpha.ai"],
+  },
 });
